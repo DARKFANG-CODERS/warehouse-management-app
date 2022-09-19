431edc4b715dfda5d1c38ac642326ec37353172e
giddob <gideonoblokuteye@gmail.com>
2022-09-19 21:31:59 +0000

    deleted some codes

--- DarkFang/settings.py
+++ DarkFang/settings.py
@@ -129,7 +129,6 @@ STATICFILES_DIRS = [
 
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
